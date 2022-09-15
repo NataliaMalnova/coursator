9961375656418c0f220ea12116ba2795ceee8c38
n.malnova <n.malnova@kaycom.ru>
2022-09-15 12:36:04 +0300

    кнопка наверх

--- src/js/events/scroll.js
+++ src/js/events/scroll.js
@@ -1,7 +1,12 @@
 window.addEventListener("scroll", (e) => {
   logicAnchorsBreadcrumbs();
   if (document.querySelector("[data-event-ftr-up]")) {
-    if (window.scrollY >= document.documentElement.clientHeight / 2) document.querySelector("[data-event-ftr-up]").classList.add("active");
-    else document.querySelector("[data-event-ftr-up]").classList.remove("active");
+    if (window.scrollY >= document.documentElement.clientHeight / 2) {
+      document.querySelector("[data-event-ftr-up]").classList.add("active");
+
+      const fixed = document.querySelector('.fixed__block-wrapper');
+      console.log(fixed)
+      if (fixed) document.querySelector("[data-event-ftr-up]").classList.add("active-fixed");
+    } else document.querySelector("[data-event-ftr-up]").classList.remove("active");
   }
 });
